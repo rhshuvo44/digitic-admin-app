e6e397f8380d6d6c5c7b9042e65aaf71c4fbdf78
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-07-11 18:08:13 +0600

    ant design charts add

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,51 @@
 import React from "react";
+import { BsArrowDownRight, BsArrowUpRight } from "react-icons/bs";
 
 const Dashboard = () => {
   return (
     <>
-      <h1>Dashboard</h1>
+      <h3 className="mb-4">Dashboard</h3>
+      <div className="d-flex justify-content-between align-items-center gap-3">
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="">Total</p>
+            <h4 className="mb-0">$1100</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="red">
+              <BsArrowDownRight />
+              32%
+            </h6>
+            <p className="mb-0">Compared To April 2022</p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="">Total</p>
+            <h4 className="mb-0">$1100</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="red">
+              <BsArrowDownRight />
+              32%
+            </h6>
+            <p className="mb-0">Compared To April 2022</p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="">Total</p>
+            <h4 className="mb-0">$1100</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="green">
+              <BsArrowUpRight />
+              32%
+            </h6>
+            <p className="mb-0">Compared To April 2022</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
