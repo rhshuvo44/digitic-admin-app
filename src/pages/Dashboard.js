f77437c0abae44385f6a679666eafc1ef0cebc51
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-07-12 13:57:15 +0600

    table for recent order

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,119 @@
+import { Column } from "@ant-design/plots";
+import { Table } from "antd";
 import React from "react";
 import { BsArrowDownRight, BsArrowUpRight } from "react-icons/bs";
 
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+  },
+  {
+    title: "Number",
+    dataIndex: "number",
+  },
+];
+const data1 = [];
+for (let i = 0; i < 46; i++) {
+  data1.push({
+    key: i,
+    name: `Edward King ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+    number: +8801784587644,
+  });
+}
 const Dashboard = () => {
+  const data = [
+    {
+      type: "January",
+      sales: 38,
+    },
+    {
+      type: "February",
+      sales: 52,
+    },
+    {
+      type: "March",
+      sales: 61,
+    },
+    {
+      type: "April",
+      sales: 145,
+    },
+    {
+      type: "May",
+      sales: 48,
+    },
+    {
+      type: "June",
+      sales: 38,
+    },
+    {
+      type: "July",
+      sales: 38,
+    },
+    {
+      type: "August",
+      sales: 38,
+    },
+    {
+      type: "September",
+      sales: 38,
+    },
+    {
+      type: "October",
+      sales: 38,
+    },
+    {
+      type: "November",
+      sales: 38,
+    },
+    {
+      type: "December",
+      sales: 38,
+    },
+  ];
+  const config = {
+    data,
+    xField: "type",
+    yField: "sales",
+    color: ({ type }) => {
+      return "#ffd333";
+    },
+    label: {
+      position: "middle",
+
+      style: {
+        fill: "#000",
+        opacity: 1,
+      },
+    },
+    xAxis: {
+      label: {
+        autoHide: true,
+        autoRotate: false,
+      },
+    },
+    meta: {
+      type: {
+        alias: "Month",
+      },
+      sales: {
+        alias: "Income",
+      },
+    },
+  };
   return (
-    <>
+    <section>
       <h3 className="mb-4">Dashboard</h3>
       <div className="d-flex justify-content-between align-items-center gap-3">
         <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
@@ -46,7 +156,19 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
-    </>
+      <div className="mt-4">
+        <h3 className="mb-4">Income statics</h3>
+        <div>
+          <Column {...config} />
+        </div>
+      </div>
+      <div className="mt-4">
+        <h3 className="mb-4">Recent Orders</h3>
+        <div>
+          <Table columns={columns} dataSource={data1} />
+        </div>
+      </div>
+    </section>
   );
 };
 
